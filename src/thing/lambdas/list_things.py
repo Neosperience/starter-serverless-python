91d4126d02106ee884db4a9c23c341cbf4a3d9bc
Dario Morandini <dario.morandini@neosperience.com>
2017-12-20 18:20:03 +0100

    Modified list-things lambda to log the error.

--- src/thing/lambdas/list_things.py
+++ src/thing/lambdas/list_things.py
@@ -1,8 +1,16 @@
 from src.container import Container
 
 
-def handler(event, context=None, container=Container()):
+def oldHandler(event, context=None, container=Container()):
     try:
         return container.thingLambdaMapper().listThings(event)
     finally:
         container.shutdown()
+def handler(event, context):
+    container = None
+    try:
+        container=Container()
+        return container.thingLambdaMapper().listThings(event)
+    finally:
+        if container:
+            container.shutdown()
